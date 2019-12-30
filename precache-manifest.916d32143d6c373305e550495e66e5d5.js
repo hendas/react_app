1af7852bb59bd5e46433cc4bc012181674e8aad6
Bartosz Marciniec <marciniec.bartosz@gmail.com>
2019-12-30 22:34:17 +0100

    Updates

---
+++ precache-manifest.916d32143d6c373305e550495e66e5d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1c0000e5d5489f329de",
+    "url": "/react_app/static/css/main.ed8d183c.chunk.css"
+  },
+  {
+    "revision": "e1c0000e5d5489f329de",
+    "url": "/react_app/static/js/main.e1c0000e.chunk.js"
+  },
+  {
+    "revision": "10c67c8a2e7bca023720",
+    "url": "/react_app/static/js/runtime~main.10c67c8a.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/react_app/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react_app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react_app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react_app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "9ce3f657a0741b78e383bd45c33c1b1e",
+    "url": "/react_app/index.html"
+  }
+];
