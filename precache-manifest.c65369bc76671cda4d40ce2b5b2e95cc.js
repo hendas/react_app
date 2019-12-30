94f9f1fb5057e58137f4f257d3d24d8af2895835
Bartosz Marciniec <marciniec.bartosz@gmail.com>
2019-12-30 21:28:58 +0100

    Updates

---
+++ precache-manifest.c65369bc76671cda4d40ce2b5b2e95cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228a310262a8feb6103b4f406f18d111",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "0fc1bd884828d38f7702",
+    "url": "/react_app/static/css/main.bec3c7a2.chunk.css"
+  },
+  {
+    "revision": "69fd2baae4cb0291e9e3",
+    "url": "/react_app/static/js/2.9ad87f1e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react_app/static/js/2.9ad87f1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fc1bd884828d38f7702",
+    "url": "/react_app/static/js/main.60ca5a71.chunk.js"
+  },
+  {
+    "revision": "127eb119e7fad8461f4a",
+    "url": "/react_app/static/js/runtime-main.b38bbcd6.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react_app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react_app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react_app/static/media/header3.d9d36b65.jpg"
+  }
+]);
